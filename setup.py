ed185c2832e9fa4f957f809ae611664f2478e435
Dr. Ben Schneider <benschneider@users.noreply.github.com>
2025-04-27 14:03:24 -0700

    v0.2.0 tag

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="llm-lucid-memory",
-    version="0.1.3",
+    version="0.2.0",
     description="Lucid Memory - Modular reasoning brain for small LLMs",
     long_description=long_description,
     long_description_content_type="text/markdown",
